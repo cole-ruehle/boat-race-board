12e0725cee14a6e8aefa4a34e20c9b98c89fdbe6
cole-ruehle <119639343+cole-ruehle@users.noreply.github.com>
2026-04-03 00:01:39 -0400

    sheet

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // "Anyone with the link" → Viewer (or broader). See SETUP.md.
 window.BDT_CONFIG = {
   // Spreadsheet ID from the URL: docs.google.com/spreadsheets/d/THIS_PART/edit
-  sheetId: "",
+  sheetId: "10chsEOPVJROgDatwGH8_gJwxj17leJXnPVrN9TBZGVE",
   // Tab id: open the tab you want; gid= in the URL (first tab is usually "0").
   gid: "0",
 };
